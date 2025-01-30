fixed normalization of the nonbacktracking matrix

functional/basic_matrix_functions.py
@@ -28,6 +28,9 @@ def create_sbm_matrix(N, m, w_ins, w_outs, seed):
     adjacency_matrix = adjacency_matrix.astype(int)
     return adjacency_matrix
 
+def get_normalization_value(i,j, C):
+    return np.sum(C[j]) - C[j,i]
+
 def construct_non_backtracking_matrix_parallel(A, flow = True):
 
     N = A.shape[0] 
@@ -50,17 +53,15 @@ def construct_non_backtracking_matrix_parallel(A, flow = True):
 
     B = np.zeros((num_edges, num_edges), dtype=np.complex128)
 
-    coefs = np.sum(A, axis = 1) - 1
     for idx1 in range(num_edges):
         i, j = edges[idx1, 0], edges[idx1, 1]
         for idx2 in range(num_edges):
             k, l = edges[idx2, 0], edges[idx2, 1]
-            if i == l and j != k:
-                if flow is True:
-                    B[idx1, idx2] = A[k,l] / coefs[i] 
+            if j == k and l != i:
+                if flow:
+                    B[idx1, idx2] = A[k,l] /  get_normalization_value(i, j, A)
                 else:
                     B[idx1, idx2] = A[k,l]
-
     return B
 
 def check_symmetric(a, rtol=1e-05, atol=1e-08):
@@ -71,9 +72,3 @@ def find_vector_y_default(x, a):
     k = a / norm_x_squared
     y = k * x
     return y
-
-def normalize(A):
-    X = A.copy()
-    X = (X.T / (np.abs(X.T).sum(0)-1)).T
-
-    return X

functional/optimization.py
@@ -1,6 +1,6 @@
 import numpy as np
 from functional.pseudohashimoto import compute_M, compute_M_weighted
-from functional.basic_matrix_functions import find_vector_y_default, normalize
+from functional.basic_matrix_functions import find_vector_y_default
 from scipy.optimize import minimize 
 
 class Optimizer:
@@ -22,6 +22,7 @@ class Optimizer:
 
         C_matrix = C_flat.reshape((n, n))
         # C_matrix = (C_matrix + C_matrix.T)/2
+
         loss = 0
         for u, K in zip(us, Ks):
             M_C_star = compute_M(u, C_matrix)
@@ -31,7 +32,8 @@ class Optimizer:
         if l < self.best[0]:
             self.best = (l,C_matrix, M_C_star, M_C, K)
             print(l)
-        
+        C_matrix = np.clip(C_matrix, 0, None)
+        # C_matrix = normalize(C_matrix)        
         return l
 
     def optimization(self):

functional/pseudohashimoto.py
@@ -1,4 +1,5 @@
 import numpy as np
+from functional.basic_matrix_functions import get_normalization_value
 
 def compute_A(u, C):
     return np.array([[u * C[i, j] / (1 - u**2 * C[i, j]**2) for j in range(C.shape[1])]  \
@@ -20,20 +21,16 @@ def compute_M(u, C):
     return I - A + D
 
 def compute_A_weighted(u, C):
-    weights = 1/(C.sum(1) - 1)
-
-    return np.array([[u * C[i, j] * weights[j] / (1 - u**2 * C[i, j] **2* weights[i]**2 ) for j in range(C.shape[1])]  \
-                     for i in range(C.shape[0])
+    return np.array([[u * C[i, j] * 1/get_normalization_value(i,j, C) / (1 - u**2 * C[i, j] **2 * 1/get_normalization_value(i,j, C)**2) for j in range(C.shape[1])]  \
+                     for i in range(C.shape[0])                                                                               # i, j 
                      ])
 
 def compute_D_weighted(u, C):
-    weights = 1/(C.sum(1) - 1)
-
     D = np.zeros_like(C, complex)
     n = D.shape[0]
     for i in range(n):
         for j in range(n):
-                D[i, i] += u**2 * C[i, j]**2  * weights[i]**2/ (1 - u**2 * C[i, j]**2 * weights[i]**2 )
+                D[i, i] += u**2 * C[i, j]**2  * 1/get_normalization_value(j,i , C)**2/ (1 - u**2 * C[i, j]**2 * 1/get_normalization_value(j, i , C)**2 )
     return D
 
 def compute_M_weighted(u, C):