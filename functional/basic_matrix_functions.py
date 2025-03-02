862e85ded39c139606148e6d8387c2f8d8ec9ccb
ArtemJDS <vasilev.artem.8.7.03@gmail.com>
2025-03-02 18:47:51 +0300

    add skew symmetric hashimoto

--- functional/basic_matrix_functions.py
+++ functional/basic_matrix_functions.py
@@ -28,8 +28,27 @@ def create_sbm_matrix(N, m, w_ins, w_outs, seed):
     adjacency_matrix = adjacency_matrix.astype(int)
     return adjacency_matrix
 
+
 def get_normalization_value(i,j, C):
     return np.sum(C[j]) - C[j,i]
+
+
+def normalize_adjacency_matrix(C):
+    normalized_C = np.zeros_like(C)
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            normalized_C[i,j] = C[i,j]/get_normalization_value(i,j, C)
+
+    return normalized_C
+
+
+def get_symmetric_component(C):
+    return 1/2 * (C + C.T)
+
+
+def get_skew_symmetric_component(C):
+    return 1/2 * (C - C.T)
+
 
 def construct_non_backtracking_matrix_parallel(A, flow = True):
 
@@ -38,7 +57,7 @@ def construct_non_backtracking_matrix_parallel(A, flow = True):
 
     for i in range(N):
         for j in range(N):
-            if A[i, j] != 0:
+            # if A[i, j] != 0:
                 num_edges += 1
 
     edges = np.zeros((num_edges, 2), dtype=np.int32)
@@ -46,7 +65,7 @@ def construct_non_backtracking_matrix_parallel(A, flow = True):
     edge_idx = 0
     for i in range(N):
         for j in range(N):
-            if A[i, j] != 0:
+            # if A[i, j] != 0:
                 edges[edge_idx, 0] = i
                 edges[edge_idx, 1] = j
                 edge_idx += 1
@@ -64,8 +83,10 @@ def construct_non_backtracking_matrix_parallel(A, flow = True):
                     B[idx1, idx2] = A[k,l]
     return B
 
+
 def check_symmetric(a, rtol=1e-05, atol=1e-08):
     return np.allclose(a, a.T, rtol=rtol, atol=atol)
+
 
 def find_vector_y_default(x, a):
     norm_x_squared = np.dot(x, x)
--- functional/pseudohashimoto.py
+++ functional/pseudohashimoto.py
@@ -21,7 +21,7 @@ def compute_M(u, C):
     return I - A + D
 
 def compute_A_weighted(u, C):
-    return np.array([[u * C[i, j] * 1/get_normalization_value(i,j, C) / (1 - u**2 * C[i, j] **2 * 1/get_normalization_value(i,j, C)**2) for j in range(C.shape[1])]  \
+    return np.array([[u * C[i, j] * 1/get_normalization_value(j, i, C) / (1 - u**2 * C[i, j] **2 * 1/get_normalization_value(j, i, C)**2) for j in range(C.shape[1])]  \
                      for i in range(C.shape[0])                                                                               # i, j 
                      ])
 
@@ -38,3 +38,29 @@ def compute_M_weighted(u, C):
     D = compute_D_weighted(u, C)
     I = np.eye(*C.shape)
     return I - A + D
+
+
+
+def compute_A_sk(u, C):
+    A = np.zeros_like(C)
+    l = A.shape[0]
+    for i in range(l):
+         for j in range(l):
+            #   print(C[i,j]**2*u**2 + 1)
+              A[i,j] = 2*C[i,j]*u/(C[i,j]**2*u**2 + 1)
+    return A
+
+
+def compute_D_sk(u, C):
+    D = np.zeros_like(C, complex)
+    n = D.shape[0]
+    for i in range(n):
+        for j in range(n):
+                D[i, i] += -2*C[i,j]**2*u**2/(C[i,j]**2*u**2 + 1)
+    return D
+
+def compute_M_sk(u, C):
+    A = compute_A_sk(u, C)
+    D = compute_D_sk(u, C)
+    I = np.eye(*C.shape)
+    return I + 1/2*(A + D)
