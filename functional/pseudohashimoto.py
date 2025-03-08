fb3beb415c974af9836469f09adc39cfc8a173ce
ArtemJDS <vasilev.artem.8.7.03@gmail.com>
2025-03-09 00:59:14 +0300

    add non-symmetric hashimoto

--- functional/pseudohashimoto.py
+++ functional/pseudohashimoto.py
@@ -1,5 +1,5 @@
 import numpy as np
-from functional.basic_matrix_functions import get_normalization_value
+from functional.basic_matrix_functions import get_symmetric_component, get_skew_symmetric_component, normalize_adjacency_matrix
 
 def compute_A(u, C):
     return np.array([[u * C[i, j] / (1 - u**2 * C[i, j]**2) for j in range(C.shape[1])]  \
@@ -20,36 +20,38 @@ def compute_M(u, C):
     I = np.eye(*C.shape)
     return I - A + D
 
-def compute_A_weighted(u, C):
-    return np.array([[u * C[i, j] * 1/get_normalization_value(j, i, C) / (1 - u**2 * C[i, j] **2 * 1/get_normalization_value(j, i, C)**2) for j in range(C.shape[1])]  \
-                     for i in range(C.shape[0])                                                                               # i, j 
-                     ])
+def compute_A_weighted(u, C_sm, C_sk):
+    A = np.zeros_like(C_sm)
+    l = A.shape[0]
+    for i in range(l):
+         for j in range(l):
+              A[i,j] = 2 * u *(C_sk[i,j]-C_sm[i,j])/(C_sk[i,j]**2*u**2 - C_sm[i,j]**2*u**2 + 1)
+    return A
 
-def compute_D_weighted(u, C):
-    D = np.zeros_like(C, complex)
+def compute_D_weighted(u, C_sm, C_sk):
+    D = np.zeros_like(C_sm, complex)
     n = D.shape[0]
     for i in range(n):
         for j in range(n):
-                D[i, i] += u**2 * C[i, j]**2  * 1/get_normalization_value(j,i , C)**2/ (1 - u**2 * C[i, j]**2 * 1/get_normalization_value(j, i , C)**2 )
+                D[i, i] += 2 * u**2 * (-C_sk[i,j]+C_sm[i,j]) * (C_sk[i,j]+C_sm[i,j])/(C_sk[i,j]**2*u**2 - C_sm[i,j]**2*u**2 + 1)
+                
     return D
 
 def compute_M_weighted(u, C):
-    A = compute_A_weighted(u, C)
-    D = compute_D_weighted(u, C)
+    C = normalize_adjacency_matrix(C)
+    C_sm, C_sk = get_symmetric_component(C), get_skew_symmetric_component(C)
+    A = compute_A_weighted(u, C_sm, C_sk)
+    D = compute_D_weighted(u, C_sm, C_sk)
     I = np.eye(*C.shape)
-    return I - A + D
-
-
+    return I + 1/2*(A + D)
 
 def compute_A_sk(u, C):
     A = np.zeros_like(C)
     l = A.shape[0]
     for i in range(l):
          for j in range(l):
-            #   print(C[i,j]**2*u**2 + 1)
               A[i,j] = 2*C[i,j]*u/(C[i,j]**2*u**2 + 1)
     return A
-
 
 def compute_D_sk(u, C):
     D = np.zeros_like(C, complex)
